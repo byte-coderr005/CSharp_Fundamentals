a95828f63c2f2e9cfed040351757ec4133bfdec6
brkoztrk07 <burak.ozturkk05@gmail.com>
2024-07-15 14:29:23 +0300

    oop devam

---
+++ OOP/Abstract/Abstract/abstract_class.cs
@@ -0,0 +1,34 @@
+﻿namespace Abstract;
+
+public class abstract_class
+{//ABSTRACT Sınıflar türetilen sınıflar için bir kalıp sağlar ve bazı 
+//metodların zorunlu olarak override edilmesini gerektirir.
+//Abstract sınıflar doğrudan nesne yaratılamayan sınıflardır.
+//Bu sınıflar diğer sınıflar için bir temel sınıf olarak kullanılır.
+}
+public abstract class Car
+{
+    public abstract void Run();
+    //ABSTRACT Metodlar, Abstract sınıfların belirli bir davranışı zorunlu kılmak istediği durumlarda
+    //KULLANILIr.
+    //Abstract metodlar ABSTRACT sınıflar içerisinde tanımlanır ve gövdesi yoktur.
+    //Bu metotlar türetilmiş sınıflardan mutlaka OVERRIDE edilmelidir.
+    public void AutomaticWiper()
+    {
+        Console.WriteLine("Automatic Wipers on.");
+    }
+}
+public class Bmw : Car
+{
+    public override void Run()
+    {
+        Console.WriteLine("SPINNING.");
+    }
+}
+public class Dacia : Car
+{
+    public override void Run()
+    {
+        Console.WriteLine("This car has no engine.");
+    }
+}
---
+++ OOP/Inheritance/inherit/SEALED.cs
@@ -0,0 +1,12 @@
+﻿namespace inherit;
+
+public class SEALED //MUHURLU CLASS
+{
+
+}
+
+public sealed class Insan 
+{
+//BU SINIF TURETILEMEZ CUNKU SEALED YAPTIK.
+}
+//public class ınsan1 : Insan
