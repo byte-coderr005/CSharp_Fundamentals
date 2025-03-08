6fb4ca7ad8d36337bf3e2e5c7dd4790a1bc8d2d3
byte-coderr005 <burak.ozturkk05@gmail.com>
2025-03-08 17:50:23 +0300

    recurs

---
+++ c01_Methods/methods05/RecursiveMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace RecursiveMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Recursive methodlar kendi içerilerinde kendilerini çağıran metodlardır.
+            int x = 10;
+            SifirAyar1(ref x);
+            Console.WriteLine(x); // çıktı 0 olacaktır.
+        }
+        public int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+        public static void SifirAyar1(ref int sayi)
+        {
+            sayi = 0;
+        }
+    }
+
+
+}
